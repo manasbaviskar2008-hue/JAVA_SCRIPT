5b37fcb66d917f7f7e82173b80696ff3de4a2fc8
manasbaviskar2008-hue <manasbavisker2008@gmail.com>
2026-06-19 10:16:14 +0530

    8 to 14 javascript

---
+++ JavaScript/7_Truthy_Falsy_JS/if_else.js
@@ -0,0 +1,17 @@
+let age = 17;
+
+if(age >= 18){
+   console.log("User can play ddlc");
+}else{
+    console.log("User can play mario");
+
+}
+
+let num = 19;
+
+if(num % 2==0){
+    console.log("even");
+}else{
+    console.log("odd");
+
+}
